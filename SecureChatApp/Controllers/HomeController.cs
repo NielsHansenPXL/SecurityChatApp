b963b0819fb011961a714121103ce80d5183d4df
Niels Hansen <niels.hansen@student.pxl.be>
2020-04-30 13:23:33 +0200

    Initial commit, basic middleware added

---
+++ SecureChatApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SecureChatApp.Models;
+
+//using SecureChatApp.ViewModels;
+
+namespace SecureChatApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           /* var homeViewModel = new HomeViewModel
+            {
+                ActiveUsers = _userRepository.ActiveUsers
+            };
+
+            return View(homeViewModel); */
+
+            var model = new User{Id = 1, UserName = "Nilson"};
+            return View("Index");
+        }
+    }
+}
